8e0747f2aa7d1164b95b29460f07a00df3936989
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-15 15:44:42 -0300

    Update ListaDeNotas.jsx

--- ceep/src/componente/ListaDeNotas.jsx
+++ ceep/src/componente/ListaDeNotas.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import CardNota from "../componente/CardNota"
+import CardNota from "./CardNota/CardNota"
 export class ListaDeNotas extends React.Component {
     render() {
         return (
             <ul>
-                {Array.of("Trabalho", "Trabalho", "Estudo").map(categoria => {
+                {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria, index) => {
                     return (
-                        <li>
+                        <li key={index}>
                             <span>{categoria}</span>
                             <CardNota />
                         </li>
