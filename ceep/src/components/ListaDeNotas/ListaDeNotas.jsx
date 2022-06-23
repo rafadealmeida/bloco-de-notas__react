5b68c1fa0c29e1f6996c88b0018bdb530436c951
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-23 18:57:53 -0300

    Update ListaDeNotas.jsx
    Colocando a do card ativando a propriedade da lista

--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -8,7 +8,10 @@ class ListaDeNotas extends Component {
         {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo = {nota.titulo} texto = {nota.texto}/>
+              <CardNota 
+              deletarNota={this.props.deletarNota}
+              titulo = {nota.titulo}
+              texto = {nota.texto}/>
             </li>
           );
         })}
