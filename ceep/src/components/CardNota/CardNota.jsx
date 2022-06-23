01d804113813b5efb0b2b0878478524de549b670
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-23 18:56:47 -0300

    Update CardNota.jsx
    1-Implementando lógica para deletar
    2-Adicionando svg via componente do react
    3-Colocando um evento de click, ele ativar uma propriedade

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg";
 class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteSVG onClick={this.props.deletarNota}/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
