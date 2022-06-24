833938b63a836b73cfac8eab3d11e22aa846fb90
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-24 14:59:12 -0300

    Criando um novo componente
    Criando o lista de categorias

---
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import "./estilo.css"
+
+class ListaDeCategorias extends Component {
+     
+    render() { 
+        return (
+            <section>
+                <ul>
+                    <li>Categorias</li>
+                    <li>Categorias</li>
+                    <li>Categorias</li>
+                    <li>Categorias</li>
+                </ul>
+            </section>
+        );
+    }
+}
+ 
+export default ListaDeCategorias;
