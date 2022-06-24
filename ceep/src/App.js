245b9f87a663758ac02762bf13ea37832c93c644
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-06-24 16:13:31 -0300

    Ajustando a formatação

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,51 +1,49 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
-import './assets/index.css';
-class App extends Component {
+import "./assets/index.css";
 
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     this.state = {
-      notas: []
-    }
+      notas: [],
+    };
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto}
-    const novoArrayNotas = [... this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas: novoArrayNotas
-    }
+      notas: novoArrayNotas,
+    };
 
-    this.setState(novoEstado)
-
+    this.setState(novoEstado);
   }
-  deletarNota(index){
-    let arrayNotas = this.state.notas
-    arrayNotas.splice(index,1)
-    
-    this.setState(arrayNotas)
-  }
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
 
+    this.setState(arrayNotas);
+  }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        
-        <main>
-          <ListaDeCategorias/>
-          <ListaDeNotas
-          deletarNota={this.deletarNota.bind(this)}
-          notas={this.state.notas}/>
-        </main>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
 
+        <main className="conteudo-principal">
+          <ListaDeCategorias />
+          <ListaDeNotas
+            deletarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}
+          />
+        </main>
       </section>
     );
   }
 }
-
 
 export default App;
