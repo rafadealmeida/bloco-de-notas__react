1a64b260f719fa6afb1863414bf28b093324e941
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-07-20 16:26:43 -0300

    Update App.js
    1-No componente de <ListaDeCategorias/> coloco o props de adicionar categorias ativando o metodo de adicionar categoria, fazendo o bind.
    2-Em seguida crio o método de adicionar categoria com o nomeCategoria como parâmetro crio o novo array de categorias, como o array antigo e adicionando  o nomeCategoria, depois, crio um novo estado e atribuo valor usando {} para ter JavaScript, assim pego o estado, a categorias dentro dele e referencio a categorias com o novo array de categorias

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -23,6 +23,13 @@ class App extends Component {
 
     this.setState(novoEstado);
   }
+
+  adicionarCategoria(nomeCategoria) {
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = {...this.state, categorias:novoArrayCategorias};
+    this.setState(novoEstado)
+  }
+
   deletarNota(index) {
     let arrayNotas = this.state.notas;
     arrayNotas.splice(index, 1);
@@ -36,7 +43,9 @@ class App extends Component {
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
 
         <main className="conteudo-principal">
-          <ListaDeCategorias categoria={this.state.categorias} />
+          <ListaDeCategorias 
+          adicionarCategoria={this.adicionarCategoria.bind(this)}
+          categoria={this.state.categorias} />
           <ListaDeNotas
             deletarNota={this.deletarNota.bind(this)}
             notas={this.state.notas}
