21b4bba19ba29846922b61231347edd76572894a
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-07-22 17:24:54 -0300

    Adicionando props de categoria

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -40,7 +40,9 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <FormularioCadastro 
+        categorias = {this.state.categorias}
+        criarNota={this.criarNota.bind(this)} />
 
         <main className="conteudo-principal">
           <ListaDeCategorias 
